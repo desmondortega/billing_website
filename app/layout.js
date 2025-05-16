5378cbe99ff2124b4f9962fccab759fe9df04a48
Matthew-Tapia <64719795+Matthew-Tapia@users.noreply.github.com>
2025-05-16 12:08:33 -0700

    completed initial navbar

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +23,38 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav style={{ padding: "1rem 0" }}>
+        <nav
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "0 4vw",
+            borderBottom: "1px solid #e5e5e5",
+            marginBottom: "2rem",
+            height: "56px",
+            minHeight: "56px",
+            color: "#555555",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <a
+              href="/"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                color: "#555555",
+              }}
+            >
+              <Image
+                src="/codex_banner.png"
+                alt="Logo"
+                width={120}
+                height={40}
+                style={{ objectFit: "contain" }}
+                priority
+              />
+            </a>
+          </div>
           <ul
             style={{
               display: "flex",
@@ -33,19 +65,29 @@ export default function RootLayout({ children }) {
             }}
           >
             <li>
-              <a href="/">Home</a>
+              <a href="/" style={{ color: "#555555" }}>
+                Home
+              </a>
             </li>
             <li>
-              <a href="/about">About us</a>
+              <a href="/about" style={{ color: "#555555" }}>
+                About us
+              </a>
             </li>
             <li>
-              <a href="/contact">Contact us</a>
+              <a href="/contact" style={{ color: "#555555" }}>
+                Contact us
+              </a>
             </li>
             <li>
-              <a href="/services">Services</a>
+              <a href="/services" style={{ color: "#555555" }}>
+                Services
+              </a>
             </li>
             <li>
-              <a href="/benefits">Benefits</a>
+              <a href="/benefits" style={{ color: "#555555" }}>
+                Benefits
+              </a>
             </li>
           </ul>
         </nav>
